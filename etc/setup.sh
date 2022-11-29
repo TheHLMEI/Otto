b3503ba7e2c7188d71463d60fb96369d96a4c058
Daniel Howell <daniel.howell@gainwelltechnologies.com>
2022-11-29 07:30:19 +0000

    add more artifacts

---
+++ etc/setup.sh
@@ -0,0 +1,14 @@
+#
+# Common Locations
+#
+export OTTOSRC=/path-to-Otto-sources
+export OTTOBIN=/path-to-Otto-sources
+export PATH=$PATH:$OTTOBIN
+
+export OTTODIR=/path-to-otto-data-files
+export OTTOCFG=$OTTODIR/otto.conf
+export OTTOLOG=$OTTODIR/log
+export OTTODB=$OTTODIR/otto.db
+export OTTOENV=$OTTODIR/otto.env
+export OTTOHTML=$OTTODIR/html
+
